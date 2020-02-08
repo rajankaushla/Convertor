67eb1631a3765d98e5b90befe70f4cbe3156c4d9
Rajan Kaushal <rajankaushal55@gmail.com>
2020-02-08 23:03:45 +0000

    first commit

---
+++ roman_numerals.rb
@@ -0,0 +1,25 @@
+
+# Programme that convert Netural Number into Roman Number
+class Fixnum  
+  def roman_numeral
+    return "The Romans have no zeros just heros. http://mathworld.wolfram.com/RomanNumerals.html " if self == 0
+    symbols = {
+              1000 => "M", 
+              500 => "D",
+              100 => "C", 
+              50 => "L", 
+              10 => "X", 
+              5 => "V", 
+              1=> "I"
+              }
+     multiplier = self
+     symbol = []
+     count = 0
+     symbols.each do |num, sym|
+       symbol.push(sym * (multiplier/num))
+       multiplier = multiplier % num
+       count += 1
+     end
+    return symbol.join
+ end
+end
---
+++ spec/roman_numeral_spec.rb
@@ -0,0 +1,28 @@
+require_relative "../roman_numerals.rb"
+
+RSpec.describe Numeric, "#roman_numeral" do
+  context "#convert number into roman numeral" do
+    it "1.roman_numeral returns I" do
+      expect(1.roman_numeral).to eq "I"
+    end
+    it "5.roman_numeral returns V" do
+      expect(5.roman_numeral).to eq "V"
+    end
+    it "10.roman_numeral returns X" do
+      expect(10.roman_numeral).to eq "X"
+    end
+    it "50.roman_numeral returns L" do
+      expect(50.roman_numeral).to eq "L"
+    end
+    it "100.roman_numeral returns C" do
+      expect(100.roman_numeral).to eq "C"
+    end
+    it "500.roman_numeral returns D" do
+      expect(500.roman_numeral).to eq "D"
+    end
+    it "1000.roman_numeral returns M" do
+      expect(1000.roman_numeral).to eq "M"
+    end
+    
+  end
+end
